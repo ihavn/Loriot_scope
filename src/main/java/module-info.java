cb1aa1eaa7a604da66267c7f0c7bf7eccd25b7ca
Ib Havn <iha@via.dk>
2022-05-17 17:32:54 +0200

    module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,7 @@ module org.edu.via.sep4.lorawan_scope.lorawan_scope {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
     requires validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
     requires java.desktop;
     requires java.net.http;
     requires org.json;
